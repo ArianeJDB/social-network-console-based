6971791e45a581ba25e206ff7e809b642f4db8e1
Ariane Jurado De Bilbao <ariane.jurado@gmail.com>
2025-03-27 16:04:41 +0100

    stores message posted with timestamp

--- src/social_network.py
+++ src/social_network.py
@@ -1,21 +1,22 @@
 
 from collections import defaultdict
-
+import datetime
 
 class SocialNetwork:
     def __init__(self):
         self.messages = defaultdict(list)
 
-    def input_manager(self):
+    def input_manager(self, get_now=datetime.datetime.now):
         command = input("> ")
 
-        self.store_message(command)
+        self.store_message(command, get_now)
         return True
     
-    def store_message(self, command):
+    def store_message(self, command, get_now=datetime.datetime.now):
         username, message = command.split(" -> ")
         self.messages[username].append({
-            "message": message
+            "message": message,
+            "timestamp": get_now()
         })
         return self.messages[username]
 
--- src/test_social_network.py
+++ src/test_social_network.py
@@ -1,20 +1,24 @@
 from collections import defaultdict
+from datetime import datetime
 import pytest
-from unittest.mock import patch
+from unittest.mock import Mock, patch
 
 from social_network import SocialNetwork
 
 
 social_network = SocialNetwork()
 
-def test_stores_message_posted():
+def test_stores_message_posted_with_timestamp():
+    fixed_time = datetime.now()
+    mock_now = Mock(return_value=fixed_time)
     social_network.messages = defaultdict(list)
+
     with patch('builtins.input', return_value="username -> test message"), \
         patch.object(social_network, 'store_message') as mock_store_message:
-            social_network.input_manager()
+            social_network.input_manager(get_now=mock_now)
 
-    mock_store_message.assert_called_once_with("username -> test message")
-    expected_message = social_network.store_message("username -> test message")
-    assert expected_message == [{"message": "test message"}]
+    mock_store_message.assert_called_once_with("username -> test message", mock_now)
+    expected_message = social_network.store_message("username -> test message", mock_now)
+    assert expected_message == [{"message": "test message", "timestamp": fixed_time}]
     assert "username" in social_network.messages
-    assert {"message": "test message"} in social_network.messages["username"]
+    assert {"message": "test message", "timestamp": fixed_time} in social_network.messages["username"]
